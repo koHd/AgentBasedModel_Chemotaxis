379f3d38c80e9b1c2ad4828be89333d539044e58
Shane Farrell <shane.farrell@yahoo.ie>
2015-11-06 13:04:03 +0000

    Testing git tracking of a c# file directly within project folder.

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : MonoBehaviour {
+
+    private float speed, maxSpeed, maxDistance;
+    private Vector3 direction;
+    private Rigidbody rb;
+    private State previousState;
+    private State currentState;
+    private State globalState;
+
+    void Start () {}
+	void Update () {}
+
+    public void setCurrentState(State newState)
+    {
+        this.currentState = newState;
+    }
+
+    public State getCurrentState()
+    {
+        return currentState;
+    }
+}
