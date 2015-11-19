be16e9a02f84faedd83c9ffe2b58241a6b06faf2
Shane Farrell <shane.farrell@yahoo.ie>
2015-11-19 12:29:31 +0000

    Sugar gradient Object Oriented design.
    Each sugar spawns four neighbour sugars that are half it's size. The neighbours are positioned at the immediate North, East, South and West in cartesian 2-D coords. When the size of the neighbours gets too small, they are no longer spawned.

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,43 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class SceneController : MonoBehaviour {
-
-    [SerializeField]
-    private GameObject sugarPrefab;
-    [SerializeField]
-    private GameObject sugarParticlePrefab;
-    private GameObject _sugar;
-    private Vector3 sugarPosition;
-    private GameObject _sugarParticle;
-    private Vector3[] neighbours;
-
-    void Start()
+namespace Assets.Scripts
+{
+    public class SceneController : MonoBehaviour
     {
-        sugarPosition = new Vector3(0, 1, 0);
-        neighbours = new Vector3[4];
-        for (int i = 0; i < neighbours.Length; i++)
-        {
-            neighbours[i] = sugarPosition;
-        }
-        neighbours[0].z++;
-        neighbours[1].x++;
-        neighbours[2].z--;
-        neighbours[3].x--;
-    }
+        [SerializeField] GameObject sugarPrefab;
+        private GameObject _sugar;
 
-    // Update is called once per frame
-    void Update ()
-    {
-	    if (_sugar == null)
+        void Update()
         {
-            _sugar = Instantiate(sugarPrefab) as GameObject;
-            _sugar.transform.position = new Vector3(0, 1, 0);
-            foreach (Vector3 position in neighbours)
+            if (_sugar == null)
             {
-                _sugarParticle = Instantiate(sugarParticlePrefab) as GameObject;
-                _sugarParticle.transform.position = position;
+                _sugar = Instantiate(sugarPrefab) as GameObject;
+                Sugar sugar = new Sugar(_sugar, 1, new Vector3(0, 1, 0));
             }
         }
-	}
+    }
 }
---
+++ Assets/Scripts/Sugar.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+    public class Sugar : MonoBehaviour
+    {
+
+        private GameObject _sugar;
+        private float size;
+        private Vector3[] neighbourSpawnPoints = new Vector3[4];
+
+        public Sugar(GameObject prefab, float size, Vector3 position)
+        {
+            this.size = size;
+            _sugar = prefab;
+            _sugar.transform.position = position;
+            _sugar.transform.localScale = new Vector3(size, size, size);
+            float neighbourSize = this.size / 2;
+            if (neighbourSize > 0.10)
+            {
+                this.buildNeighbourhood();
+                this.spawnNeighbours();
+            }
+        }
+
+        public void buildNeighbourhood()
+        {
+            for (int i = 0; i < neighbourSpawnPoints.Length; i++)
+            {
+                neighbourSpawnPoints[i] = _sugar.transform.position;
+            }
+            neighbourSpawnPoints[0].z++;
+            neighbourSpawnPoints[1].x++;
+            neighbourSpawnPoints[2].z--;
+            neighbourSpawnPoints[3].x--;
+        }
+
+        public void spawnNeighbours()
+        {
+            foreach (Vector3 spawnPoint in neighbourSpawnPoints)
+            {
+                Sugar neighbour = new Sugar(_sugar, _sugar.transform.localScale.x, spawnPoint);
+            }
+        }
+
+        public GameObject getSugar()
+        {
+            return _sugar;
+        }
+    }
+}
