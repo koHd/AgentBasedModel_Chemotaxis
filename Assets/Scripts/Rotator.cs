6d2948858d477d0c52c208b5ab5a17690b1f9719
Shane Farrell <shane.farrell@yahoo.ie>
2015-11-19 00:05:17 +0000

    Added SceneController for quick prototype diffusion.
    https://trello.com/c/JMnqi6pV
    
    Prototype of diffusion with object oriented design in mind. This prototype places smaller particles of sugar at all free North East South West positions around main sugar molecule.
    When implemented fully, you will have a radial pattern of smaller sugar particles surrounded by even smaller sugar particles. Giving us the desired chemical gradient model.

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,11 +2,10 @@
 using System.Collections;
 
 public class Rotator : MonoBehaviour {
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
 		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
-
 	}
 }
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SceneController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject sugarPrefab;
+    [SerializeField]
+    private GameObject sugarParticlePrefab;
+    private GameObject _sugar;
+    private Vector3 sugarPosition;
+    private GameObject _sugarParticle;
+    private Vector3[] sugarGradient;
+
+    void Start()
+    {
+        sugarPosition = new Vector3(0, 1, 0);
+        sugarGradient = new Vector3[4];
+        for (int i = 0; i < sugarGradient.Length; i++)
+        {
+            sugarGradient[i] = sugarPosition;
+        }
+        sugarGradient[0].z++;
+        sugarGradient[1].x++;
+        sugarGradient[2].z--;
+        sugarGradient[3].x--;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	    if (_sugar == null)
+        {
+            _sugar = Instantiate(sugarPrefab) as GameObject;
+            _sugar.transform.position = new Vector3(0, 1, 0);
+            foreach (Vector3 position in sugarGradient)
+            {
+                _sugarParticle = Instantiate(sugarParticlePrefab) as GameObject;
+                _sugarParticle.transform.position = position;
+            }
+        }
+	}
+}
