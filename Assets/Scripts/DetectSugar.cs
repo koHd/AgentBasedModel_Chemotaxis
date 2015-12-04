8f8c22d8317f03b4b8dfcbb8e3e9a289be36c7b2
Shane Farrell <shane.farrell@yahoo.ie>
2015-12-04 11:05:42 +0000

    New sugar detection system for agent.
    New approach: Model sugar as a radial zone. Have agent detect first collision with sugar and from that point on check the sugar concentration at co-ord. Sugar concentration still needs to be mapped on to the radial zone. Currently using a GameObject created in the Unity3d visual editor. Need to move to programmatically creating the SugarGradient GameObject and map concentration values.

---
+++ Assets/Scripts/DetectSugar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectSugar : MonoBehaviour
+{
+    bool inSugar = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Lookin' for sugar...");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (inSugar)
+            Debug.Log("Sugar at " + transform.position);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<SugarGradient>())
+            inSugar = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (inSugar)
+            inSugar = false;
+    }
+}
---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveForward : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Something good up ahead!");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += Vector3.forward * Time.deltaTime;
+	}
+}
