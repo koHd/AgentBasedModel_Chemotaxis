db3f9fe2df9a42e91dd5e2a1d2e54a2414f41ed6
Shane Farrell <shane.farrell@yahoo.ie>
2015-12-08 15:49:19 +0000

    Assed BacterialChemotaxis script.
    Basic model of bacterial chemotaxis. Still need to implement the tumble method.

---
+++ Assets/Scripts/BacterialChemotaxis.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BacterialChemotaxis : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GetComponent<DetectSugar>().isInSugar())
+        {
+            swim();
+        } else
+        {
+            tumble();
+        }
+    }
+
+    private void swim()
+    {
+        Debug.Log("Swimming...");
+        transform.position += Vector3.forward * Time.deltaTime;
+    }
+
+    private void tumble()
+    {
+        Debug.Log("Tumbling...");
+    }
+}
