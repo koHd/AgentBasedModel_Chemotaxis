79e3671e4cc565c852c38a371fd4b7eb774b8675
Shane Farrell <shane.farrell@yahoo.ie>
2016-03-23 18:29:57 +0000

    Dynamically create a few different Chemicals.
    Moving towards a system to allow the user to place Chemicals around the scene and choose their parameters.

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -8,8 +8,7 @@ public class SceneController : MonoBehaviour
     float timeElapsed;
     [SerializeField]
     private GameObject ecoliPrefab, checmicalPrefab;
-    private GameObject _chemical;
-    private GameObject[] ecoli;
+    private GameObject[] ecoli, chemicals;
     private int numEcoliInChemical;
 
     void Start()
@@ -20,11 +19,27 @@ public class SceneController : MonoBehaviour
         for (int i = 0; i < ecoli.Length; i++)
         {
             ecoli[i] = Instantiate(ecoliPrefab) as GameObject;
-            ecoli[i].transform.position = new Vector3(0, 0.5f, -40f);
+            ecoli[i].transform.position = new Vector3(Random.Range(-40.0f, 40.0f), 0.5f, Random.Range(-40.0f, 40.0f));
         }
-        _chemical = Instantiate(checmicalPrefab) as GameObject;
-        _chemical.transform.localScale += new Vector3(50.0f, 0, 50.0f);
-        _chemical.GetComponent<Chemical>().setChemotaxisType(true);
+        chemicals = new GameObject[5];
+        for (int i = 0; i < chemicals.Length; i++)
+        {
+            chemicals[i] = Instantiate(checmicalPrefab) as GameObject;
+            chemicals[i].transform.position = new Vector3(Random.Range(-100.0f, 100.0f), 0.0f, Random.Range(-100.0f, 100.0f));
+            float radius = Random.Range(20.0f, 80.0f);
+            chemicals[i].transform.localScale += new Vector3(radius, 0.0f, radius);
+            float cointToss = Random.Range(0.0f, 1.0f);
+            if (cointToss >= 0.5f)
+            {
+                chemicals[i].GetComponent<Chemical>().setChemotaxisType(true);
+                Shader shader1 = Shader.Find("Attractant");
+                chemicals[i].GetComponent<Renderer>().material.shader = shader1;
+            }
+            else
+            {
+                chemicals[i].GetComponent<Chemical>().setChemotaxisType(true);
+            }
+        }
     }
 
     void Update()
