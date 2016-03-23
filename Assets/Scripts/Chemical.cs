f4523178396c2f6c6d765535e3f931bc90b2379d
Shane Farrell <shane.farrell@yahoo.ie>
2016-03-23 12:35:23 +0000

    Added Chemical script.
    Replacing SugarGradient script with a more generic Chemical that can act as an attractant or a repellent.

---
+++ Assets/Scripts/Chemical.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chemical : MonoBehaviour {
+
+    private Vector3 origin;
+    private float concentration;
+    private bool attractant;
+
+    // Use this for initialization
+    void Start()
+    {
+        origin = transform.position;
+    }
+
+    public float getConcentration(Vector3 position)
+    {
+        return transform.localScale.magnitude / Vector3.Distance(origin, position);
+    }
+}
