cb4af498c3fc6371db1692bce32cabaddbcf2da3
Shane Farrell <shane.farrell@yahoo.ie>
2016-03-24 14:58:42 +0000

    Refactored.
    Tidied up Start method by moving large chunks of code into stand alone methods.

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -9,46 +9,56 @@ public class SceneController : MonoBehaviour
     [SerializeField]
     private GameObject ecoliPrefab, checmicalPrefab;
     private GameObject[] ecoli, chemicals;
-    private int numEcoliInChemical;
+    private int numEcoli, numChemicals, numEcoliInChemical;
 
     void Start()
     {
-        numEcoliInChemical = 0;
         startTime = Time.time;
-        ecoli = new GameObject[50];
-        for (int i = 0; i < ecoli.Length; i++)
-        {
-            ecoli[i] = Instantiate(ecoliPrefab) as GameObject;
-            ecoli[i].transform.position = new Vector3(Random.Range(-40.0f, 40.0f), 0.5f, Random.Range(-40.0f, 40.0f));
-        }
-        chemicals = new GameObject[5];
-        for (int i = 0; i < chemicals.Length; i++)
-        {
-            chemicals[i] = Instantiate(checmicalPrefab) as GameObject;
-            chemicals[i].transform.position = new Vector3(Random.Range(-100.0f, 100.0f), 0.0f, Random.Range(-100.0f, 100.0f));
-            float radius = Random.Range(20.0f, 80.0f);
-            chemicals[i].transform.localScale += new Vector3(radius, 0.0f, radius);
-            float cointToss = Random.Range(0.0f, 1.0f);
-            if (cointToss >= 0.5f)
-            {
-                chemicals[i].GetComponent<Chemical>().setChemotaxisType(true);
-                Shader shader1 = Shader.Find("Attractant");
-                chemicals[i].GetComponent<Renderer>().material.shader = shader1;
-            }
-            else
-            {
-                chemicals[i].GetComponent<Chemical>().setChemotaxisType(true);
-            }
-        }
+        numEcoli = 20;
+        numChemicals = 1;
+        numEcoliInChemical = 0;
+        createEcoli(numEcoli);
+        createChemicals(numChemicals);
     }
 
     void Update()
     {
-        if (numEcoliInChemical != Ecoli.getNumInChemical())
+        if (numEcoliInChemical != Ecoli.getNumInAttractant())
         {
-            numEcoliInChemical = Ecoli.getNumInChemical();
-            Debug.Log("Number of E. coli in chemical: " + numEcoliInChemical);
+            numEcoliInChemical = Ecoli.getNumInAttractant();
+            Debug.Log("Number of E. coli in attractant: " + numEcoliInChemical);
         }
         timeElapsed = timeElapsed - startTime;
+    }
+
+    public void createEcoli(int numEcoli)
+    {
+        ecoli = new GameObject[numEcoli];
+        for (int i = 0; i < ecoli.Length; i++)
+        {
+            ecoli[i] = Instantiate(ecoliPrefab) as GameObject;
+            ecoli[i].transform.position = new Vector3(Random.Range(-10.0f, 10.0f), 0.5f, Random.Range(-10.0f, 10.0f));
+        }
+    }
+
+    public void createChemicals(int numChemicals)
+    {
+        chemicals = new GameObject[numChemicals];
+        for (int i = 0; i < chemicals.Length; i++)
+        {
+            chemicals[i] = Instantiate(checmicalPrefab) as GameObject;
+            chemicals[i].transform.position = new Vector3(Random.Range(-100.0f, 100.0f), 0.0f, Random.Range(-100.0f, 100.0f));
+            float radius = Random.Range(50.0f, 200.0f);
+            chemicals[i].transform.localScale += new Vector3(radius, 0.0f, radius);
+            float cointToss = 1.0f;
+            if (cointToss >= 0.5f)
+            {
+                chemicals[i].GetComponent<Chemical>().setChemotaxisType(true);
+            }
+            else
+            {
+                chemicals[i].GetComponent<Chemical>().setChemotaxisType(false);
+            }
+        }
     }
 }
