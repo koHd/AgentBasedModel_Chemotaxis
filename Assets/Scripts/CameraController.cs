94028d060ef70fc09181028e18e2c80255b6e123
Shane Farrell <shane.farrell@yahoo.ie>
2016-04-09 16:29:55 +0100

    Added microscope control.
    Camera is manipulated like microscope, using the wasd keys on the keyboard for movement and zoom.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    private float zoomSpeed = 5f, moveSpeed = 100f;
+	
+	void Update () {
+        if (Input.GetKey("w"))
+            Camera.main.orthographicSize += zoomSpeed;
+        if (Input.GetKey("s"))
+            Camera.main.orthographicSize -= zoomSpeed;
+        if (Input.GetKey("a"))
+            transform.localPosition += -Vector3.right * Time.deltaTime * moveSpeed;
+        if (Input.GetKey("d"))
+            transform.localPosition += Vector3.right * Time.deltaTime * moveSpeed;
+    }
+}
